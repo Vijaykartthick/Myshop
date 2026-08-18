3a312f2bcafb0d7997f12ce507241a2ab42f115d
Your Name <you@example.com>
2026-08-18 22:49:56 +0530

    Enable debug for Vercel error

--- myapp/settings.py
+++ myapp/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".vercel.app",
