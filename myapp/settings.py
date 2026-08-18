b5d0a2957c98c03cf712c7709d75848b8d90143b
Your Name <you@example.com>
2026-08-18 22:39:50 +0530

    Allow Vercel host

--- myapp/settings.py
+++ myapp/settings.py
@@ -86,10 +86,9 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
