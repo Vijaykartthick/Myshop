215da133cb2e41b56900dd14fb6d96a0c62f9e72
Your Name <you@example.com>
2026-08-18 22:47:25 +0530

    Use writable SQLite path on Vercel

--- myapp/settings.py
+++ myapp/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r!d*c^gd5ypgw6*^cx@7w#^+p+a43z%a7@9*gv6^n1s01i26_k'
+SECRET_KEY = 'django-insecure-...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
