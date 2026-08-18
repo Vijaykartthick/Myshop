00862c0bb9bab22e60f69a5a9328a601950fe827
Your Name <you@example.com>
2026-08-18 22:44:27 +0530

    Use writable SQLite path on Vercel

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,13 +82,18 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+        'NAME': (
+            '/tmp/db.sqlite3'
+            if os.environ.get('VERCEL')
+            else BASE_DIR / 'db.sqlite3'
+        ),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
 
